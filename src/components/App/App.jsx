a2730641db4ca9fc733b029560fc4e51b8e9e5d7
Sam Johnson <s_johnso@yahoo.com>
2023-08-12 17:34:23 -0500

    added like button and counter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,14 @@ import GalleryList from './GalleryList/GalleryList';
 
 
 function App() {
-// renders images when the page loads
+  // renders images when the page loads
   useEffect(() => {
-    fetchImages()
+    fetchImages();
   }, [])
-// imageGallery is an array with all the images
+  // imageGallery is an array with all the images
   let [imageGallery, setImageGallery] = useState();
 
-// GET images from the database and set them into imageGallery array
+  // GET images from the database and set them into imageGallery array
   const fetchImages = () => {
     axios.get(`/gallery`)
       .then((response) => {
@@ -26,12 +26,27 @@ function App() {
       })
   }
 
+  const addLike = (galleryItem) => {
+    console.log('likedImage: ', galleryItem)
+
+    axios.put(`/gallery/like/${galleryItem.id}`, {likes: galleryItem})
+      .then((response) => {
+        console.log('Like added: ', galleryItem.id);
+        fetchImages();
+      })
+      .catch((error) => {
+        console.log('Error in PUT/ gallery', error)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Gallery of My Life</h1>
       </header>
-      <GalleryList imageGallery={imageGallery} fetchImages={fetchImages}/>
+      <GalleryList
+        imageGallery={imageGallery} 
+        addLike={addLike} />
     </div>
   );
 }
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -1,16 +1,17 @@
 
 import GalleryItem from '../GalleryItem/GalleryItem';
 
-function GalleryList({ imageGallery, fetchImages }) {
-    
+function GalleryList({ imageGallery, addLike }) {
+
     return (
         <div>
             {/* loops over the imageGallery and turns each into a galleryItem */}
             {imageGallery?.map((image) => (
-                <GalleryItem 
-                id={image.id}
-                galleryItem={image} 
-                fetchImages={fetchImages}/>
+                <GalleryItem
+                    key={image.id}
+                    galleryItem={image}
+                    addLike={addLike}
+                />
             ))}
         </div>
     )
