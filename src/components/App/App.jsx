aa1510e6a30cbb04118ddc32841a0da23025b5d0
Sam Johnson <s_johnso@yahoo.com>
2023-08-12 11:18:29 -0500

    Created gallery list and gallery item components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,39 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+import GalleryList from './GalleryList/GalleryList';
+
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+// renders images when the page loads
+  useEffect(() => {
+    fetchImages()
+  }, [])
+// imageGallery is an array with all the images
+  let [imageGallery, setImageGallery] = useState();
+
+// GET images from the database and set them into imageGallery array
+  const fetchImages = () => {
+    axios.get(`/gallery`)
+      .then((response) => {
+        setImageGallery(response.data)
+        console.log("ImageGallery: ", response.data)
+      })
+      .catch((error) => {
+        console.log('Error in GET/ gallery', error)
+      })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <GalleryList imageGallery={imageGallery}/>
+    </div>
+  );
 }
 
 export default App;
