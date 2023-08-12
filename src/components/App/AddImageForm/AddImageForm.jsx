e4a076f760cb602afeaa0c993d35135082058c66
Sam Johnson <s_johnso@yahoo.com>
2023-08-12 18:23:10 -0500

    added form to add new image

---
+++ src/components/App/AddImageForm/AddImageForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+function AddImageForm({ addNewImage }) {
+    // state variables that relate to the input fields of the form
+    let [newImageDescription, setNewImageDescription] = useState('');
+    let [newImagePath, setNewImagePath] = useState('');
+
+    // on submit click package state variables and call function for POST on app.jsx
+    const handleSubmit = (event) => {
+        event.preventDefault();
+console.log(newImageDescription, newImagePath);
+        // make an object with the new image state variables
+        const newImage = {
+            description: newImageDescription,
+            path: newImagePath
+        };
+        console.log('new image in submit', newImage)
+        addNewImage(newImage, clearInputs);
+    }
+
+    // reset the state to clear the input fields
+    const clearInputs = () => {
+        setNewImageDescription('');
+        setNewImagePath('');
+    }
+
+    return (
+        // form to add a new image
+        <form onSubmit={handleSubmit}>
+            <label>Description:</label>
+            <input
+                type='text'
+                name='description'
+                value={newImageDescription}
+                onChange={(event) => setNewImageDescription(event.target.value)}
+            />
+
+            <label>URL:</label>
+            <input
+                type='text'
+                name='URL'
+                value={newImagePath}
+                onChange={(event) => setNewImagePath(event.target.value)}
+            />
+            <button type='submit'>Add Item</button>
+        </form>
+    )
+}
+
+export default AddImageForm
