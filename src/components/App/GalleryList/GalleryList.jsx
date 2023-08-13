244ae8b466bcce17021691dab34896cf2864c7f1
Sam Johnson <s_johnso@yahoo.com>
2023-08-13 14:12:47 -0500

    Updated image grid pattern

--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -1,5 +1,17 @@
 
 import GalleryItem from '../GalleryItem/GalleryItem';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 function GalleryList({ imageGallery, addLike, deleteImage }) {
 
@@ -7,12 +19,15 @@ function GalleryList({ imageGallery, addLike, deleteImage }) {
         <>
             {/* loops over the imageGallery and turns each into a galleryItem */}
             {imageGallery?.map((image) => (
-                    <GalleryItem
+                <Grid itemxs={4} key={image.id}>
+                    <Item><GalleryItem
                         key={image.id}
                         galleryItem={image}
                         addLike={addLike}
                         deleteImage={deleteImage}
                     />
+                    </Item>
+                </Grid>
             ))}
         </>
     )
