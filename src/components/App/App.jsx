61704c0d770fcba2aca5f379a0027786ac6efe47
Sam Johnson <s_johnso@yahoo.com>
2023-08-13 13:34:02 -0500

    added Card styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import GalleryList from './GalleryList/GalleryList';
 import AddImageForm from './AddImageForm/AddImageForm';
 
--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,4 +1,13 @@
 import { useState } from "react"
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from "@mui/material";
+import Typography from '@mui/material/Typography';
+
 
 function GalleryItem({ galleryItem, addLike, deleteImage }) {
     // console.log('galleryItem in GalleryItem', galleryItem)
@@ -8,23 +17,35 @@ function GalleryItem({ galleryItem, addLike, deleteImage }) {
     const [itemDisplay, setItemDisplay] = useState(true)
 
     return (
-        <>
-            <div onClick={() => {
+        <Card sx={{ maxWidth: 245 }} >
+            <CardActionArea>
+                <div onClick={() => {
                 setItemDisplay(!itemDisplay)
             }}>
                 {itemDisplay ?
-                    <img src={galleryItem.path} />
+                    <CardMedia
+                        sx={{ height: 245 }}
+                        image={galleryItem.path}
+                        title="Goat"
+                    />
                     :
-                    <div>{galleryItem.description}</div>}
+                    <CardContent sx={{ height: 205 }} >
+                        <Typography variant="h6" component="div" backgroundColor="yellow">
+                            Image Description:
+                        </Typography>
+                        <Typography color="text.secondary" backgroundColor="yellow">
+                            {galleryItem.description}
+                            </Typography>
+                    </CardContent>}
             </div>
-            <div>
-                <button onClick={() => addLike(galleryItem)}>Like</button>
-                <span>likes: {galleryItem.likes}</span>
-            </div>
-            <div>
-                <button onClick={() => deleteImage(galleryItem)}>Remove Image</button>
-            </div>
-        </>
+            </CardActionArea>
+            <CardActions>
+                <Button onClick={() => addLike(galleryItem)} variant="text" color="secondary" size="small" padding="5px">Like</Button>
+                <Typography>{galleryItem.likes} Likes</Typography>          
+                <Button onClick={() => deleteImage(galleryItem)} color="error" startIcon={<DeleteIcon />} size="small" >Remove</Button>
+            </CardActions>
+
+        </Card>
     )
 }
 
